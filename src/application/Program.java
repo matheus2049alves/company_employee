c90bcaa1bd6a808cafe02a1fd7ee0ed1a1e44793
matheus2049alves <mathii10costa@gmail.com>
2023-10-23 18:00:18 -0300

    adicionado as entradas de dados e impressão dos mesmos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 import entities.Employee;
+import entities.OutsourcedEmployee;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -15,13 +17,32 @@ public class Program {
 
         List<Employee> list = new ArrayList<>();
 
-        for (int i = 0; i < number; i++){
-            System.out.println("Employee #"+(i+ 1) + " data: ");
-            
+        for (int i = 1; i <= number; i++){
+            input.nextLine();
+            System.out.println("Employee #"+i+  " data: ");
+            System.out.println("Outsourced? (Y/N)");
+            char response = input.next().charAt(0);
+            System.out.println("Name: ");
+            input.nextLine();
+            String name = input.nextLine();
+            System.out.println("Hours: ");
+            int hours = input.nextInt();
+            System.out.println("value per hour: ");
+            double valuePerHour = input.nextDouble();
+            if (response == 'y'){
+                System.out.println("additional charge: ");
+                double additionalCharge = input.nextDouble();
+                list.add(new OutsourcedEmployee(name,hours,valuePerHour,additionalCharge));
+            }else {
+                list.add(new Employee(name, hours, valuePerHour));
+            }
+
         }
-
-
-
-
+        System.out.println();
+        System.out.println("PAYMENTS: ");
+        for(Employee emp : list){
+            System.out.println(emp.getName() +"- $"+ emp.payment());
+        }
+        input.close();
     }
 }
